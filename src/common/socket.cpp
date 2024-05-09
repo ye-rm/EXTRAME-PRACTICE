3f290b164c7a1a2ab6ebf121d9c2ce20dcf67c02
Wenyu Ye <wenyuye@bupt.edu.cn>
2024-05-09 17:54:05 +0800

    read ip and sub_id from .csv

--- src/common/socket.cpp
+++ src/common/socket.cpp
@@ -1,14 +1,11 @@
 #include "socket.h"
-#include<iostream>
-#include<filesystem>
+
 int Socket::init_ip() {
-    //print current directory
-    // std::cout << "current directory: " << std::filesystem::current_path() << std::endl;
-    // rapidcsv::Document doc("ip.csv");
-    // server_ip = SERVER_IP;
-    // for (int i = 1; i < doc.GetRowCount(); i++) {
-    //     sub_ip_map[doc.GetCell<int>(i, 1)] = doc.GetCell<std::string>(i, 2);
-    // }
+    rapidcsv::Document doc("/home/wenyuye/C_Projects/EXTRAME-PRACTICE/iptable.csv");
+    server_ip = SERVER_IP;
+    for (int i = 0; i < doc.GetRowCount(); i++) {
+        sub_ip_map[doc.GetCell<int>(1, i)] = doc.GetCell<std::string>(2, i);
+    }
     return 0;
 }
 
@@ -22,32 +19,38 @@ Socket::Socket(int s_id) {
     WSAStartup(MAKEWORD(2, 2), &wsaData);
 #endif
     init_ip();
-    // sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    // if (sockfd < 0) {
-    //     perror("socket error");
-    //     exit(1);
-    // }
-    // if (s_id == 0) {
-    //     struct sockaddr_in addr;
-    //     addr.sin_family = AF_INET;
-    //     addr.sin_port = htons(PORT);
-    //     addr.sin_addr.s_addr = inet_addr(server_ip.c_str());
-    //     if (connect(sockfd, (struct sockaddr *) &addr, sizeof(addr)) < 0) {
-    //         perror("server socket error, unable to connect");
-    //         return;
-    //     }
-    // } else {
-    //     struct sockaddr_in addr;
-    //     addr.sin_family = AF_INET;
-    //     addr.sin_port = htons(PORT);
-    //     addr.sin_addr.s_addr = inet_addr(sub_ip_map[s_id].c_str());
-    //     if (bind(sockfd, (struct sockaddr *) &addr, sizeof(addr)) < 0) {
-    //         perror("client socket error, unable to bind");
-    //         return;
-    //     }
-    // }
+    sockfd = socket(AF_INET, SOCK_STREAM,0 );
+    if (sockfd < 0) {
+        perror("socket error");
+        exit(1);
+    }
+    if (s_id == 0) {
+        // server socket
+        struct sockaddr_in addr;
+        addr.sin_family = AF_INET;
+        addr.sin_port = htons(PORT);
+        addr.sin_addr.s_addr = inet_addr(server_ip.c_str());
+        if (bind(sockfd, (struct sockaddr *) &addr, sizeof(addr)) < 0) {
+            perror("server socket error, unable to bind");
+            return;
+        }
+    } else {
+        struct sockaddr_in addr;
+        addr.sin_family = AF_INET;
+        addr.sin_port = htons(PORT);
+        addr.sin_addr.s_addr = inet_addr(sub_ip_map[s_id].c_str());
+        if (bind(sockfd, (struct sockaddr *) &addr, sizeof(addr)) < 0) {
+            perror("client socket error, unable to bind");
+            return;
+        }
+    }
 }
 
 Socket::~Socket() {
-
+#if defined(_WIN32)
+    closesocket(sockfd);
+    WSACleanup();
+#else
+    close(sockfd);
+#endif 
 }
